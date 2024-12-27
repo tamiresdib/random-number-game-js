65fee0afd9d0dd89b0aa2ca411c904be21ad844d
Tamires Dib <tamiresdib6@gmail.com>
2024-12-26 23:02:14 -0300

    feat: inclusion array list and responsive voice

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, `Brazilian Portuguese Female`, {rate:1.2});
 }
 
 function exibirMensagemInicial() {
@@ -34,7 +37,19 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 3 + 1);
+    let numeroEscolhido =  parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementoNaLista =  listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementoNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido; 
+    }
 }
 
 function limparCampo() {
@@ -49,3 +64,4 @@ function reiniciarJogo() {
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
+
